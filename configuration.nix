2320cb78341dddd0f819506925bc7d6effa84fcd
JSH4a <jh3394@bath.ac.uk>
2024-07-27 14:40:48 +0100

    Added support for flakes

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,9 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Set your time zone.
   time.timeZone = "Europe/London";
 
